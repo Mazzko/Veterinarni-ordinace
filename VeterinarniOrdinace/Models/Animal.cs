c4722d94d85b0f14a53393c3df89ccefed157462
Mazzko <jangatner@gmail.com>
2025-11-11 10:19:32 +0100

    vytvoření základní struktury projektu s pohledy a modely

---
+++ VeterinarniOrdinace/Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinarniOrdinace.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Species { get; set; } = string.Empty;
+        public string? Breed { get; set; }
+        public int OwnerId { get; set; } //FK
+    }
+}
---
+++ VeterinarniOrdinace/Models/Visit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinarniOrdinace.Models
+{
+    public class Visit
+    {
+        public int Id { get; set; }
+        public int AnimalId { get; set; } //FK
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Reason { get; set; } = String.Empty;
+        public string? Diagnosis { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; } = "Naplánováno"; // Naplánováno / Dokončeno / Zrušeno
+    }
+}
